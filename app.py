950acaf181d8124709bc4b2a9126b9a280581497
Pierre-louis Sans <pierre.louis.sans@gmail.com>
2023-04-22 18:21:32 +0200

    individual page

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ app.config['DEBUG'] = True
 def index():
     return render_template('index.html')
 
+@app.route('/details.html')
+def details():
+    return render_template('details.html')
+
 
 @app.route('/api/monsters')
 def get_monsters():
@@ -18,3 +22,13 @@ def get_monsters():
     data = response.json()
     return jsonify(data)
 
+@app.route('/api/monsters/<string:name>')
+def get_monster_by_name(name):
+    url = 'https://raw.githubusercontent.com/Pl83/Mh-Monster-data/main/data.json'
+    response = requests.get(url)
+    datas = response.json()
+    for data in datas['Monsters']:
+        if data['name'] == name:
+            return jsonify(data)
+    return jsonify({'error': 'Monster not found'})
+    
