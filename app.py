e8a100662b58861c0a4626bb5b2775930cd24fa1
Pierre-louis Sans <pierre.louis.sans@gmail.com>
2023-04-23 22:05:30 +0200

    d

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
-from flask import Flask, render_template, jsonify
-import requests
-import jinja2 as jinja2
+from flask import Flask, render_template, jsonify # pip install flask 
+import requests # pip install requests
+import jinja2 as jinja2 # pip install jinja2  (useless to call him here, but it's for the record)
 
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-# template road
+
+# template road (to associate the html files to the app)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/monsters.html')
+def monsters():
+    return render_template('monsters.html')
 
 @app.route('/details.html')
 def details():
@@ -19,9 +24,13 @@ def details():
 def weapons():
     return render_template('weapons.html')
 
+@app.route('/type-list.html')
+def type_list():
+    return render_template('type-list.html')
 
 
-# data road
+
+# data road (to make data accessible on all pages)
 @app.route('/api/monsters')
 def get_monsters():
     url = 'https://raw.githubusercontent.com/Pl83/Mh-Monster-data/main/data_monsters.json'
