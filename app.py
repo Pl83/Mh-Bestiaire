611e24618257c5f0fc2e5d8c3dece848dbb494a7
Pierre-louis Sans <pierre.louis.sans@gmail.com>
2023-04-23 19:22:18 +0200

    f

--- app.py
+++ app.py
@@ -15,19 +15,23 @@ def index():
 def details():
     return render_template('details.html')
 
+@app.route('/weapons.html')
+def weapons():
+    return render_template('weapons.html')
+
 
 
 # data road
 @app.route('/api/monsters')
 def get_monsters():
-    url = 'https://raw.githubusercontent.com/Pl83/Mh-Monster-data/main/data.json'
+    url = 'https://raw.githubusercontent.com/Pl83/Mh-Monster-data/main/data_monsters.json'
     response = requests.get(url)
     data = response.json()
     return jsonify(data)
 
 @app.route('/api/monsters/<string:name>')
 def get_monster_by_name(name):
-    url = 'https://raw.githubusercontent.com/Pl83/Mh-Monster-data/main/data.json'
+    url = 'https://raw.githubusercontent.com/Pl83/Mh-Monster-data/main/data_monsters.json'
     response = requests.get(url)
     datas = response.json()
     for data in datas['Monsters']:
@@ -35,3 +39,10 @@ def get_monster_by_name(name):
             return jsonify(data)
     return jsonify({'error': 'Monster not found'})
     
+
+@app.route('/api/weapons')
+def get_weapons():
+    url = 'https://raw.githubusercontent.com/Pl83/Mh-Monster-data/main/data_weapons.json'
+    response = requests.get(url)
+    data = response.json()
+    return jsonify(data)
